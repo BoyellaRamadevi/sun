27dbc09913833a25b8821dbed1fb85444e2dbcc8
BoyellaRamadevi <41728410+BoyellaRamadevi@users.noreply.github.com>
2018-07-27 15:36:33 +0530

    Create sun.java

---
+++ sun.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner.*;
+/* Name of the class has to be "Main" only if athe class is public. */
+class Idea
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		if(num % 4 == 0)
+		       {
+		System.out.println("yes");
+		       }
+		     else
+	           {
+			System.out.println("no");
+	           }
+	    
+            
+
+	}
+}
